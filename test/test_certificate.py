4e95380c29fd903287db1567fa59a0ff12279338
JINM0RI <srivathsan.r2003@gmail.com>
2026-02-08 17:42:43 +0530

    fixed bugs

--- test/test_certificate.py
+++
@@ -1,3 +0,0 @@
-from modules.certificate_generator import generate_certificate
-
-generate_certificate("Test User")
--- test/test_pdf.py
+++
@@ -1,6 +0,0 @@
-from modules.pdf_generator import image_to_pdf
-
-# Make sure this image exists from Module 3
-image_path = "output/Test User.png"
-
-image_to_pdf(image_path)
